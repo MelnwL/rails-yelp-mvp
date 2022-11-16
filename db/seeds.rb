17e0a7ca5006b6b49686e8e3a86f735033f8053e
Melanie Marechal <mel2012@outlook.fr>
2022-11-16 16:46:17 +0100

    rails yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+8.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Internet.domain_name,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: Restaurant::CATEGORY.sample
+    # va chercher la constante CATEGORY ds la table restaurant
+  )
+  restaurant.save!
+
+    6.times do
+      review = Review.new(
+        rating: rand(0..5),
+        content: Faker::Restaurant.review,
+        restaurant_id: restaurant.id
+      )
+      review.save!
+  end
+  puts 'Finished!'
+end
